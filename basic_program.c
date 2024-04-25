ecc6068d8aca52f9154a4c3086b8f92109f69121
asifmahmud35 <asif.mahmud@brainstation-23.com>
2024-04-25 13:52:15 +0600

    Extend programming problem to include Fibonacci sequence

--- basic_program.c
+++ basic_program.c
@@ -8,11 +8,31 @@ int factorial(int n) {
         return n * factorial(n - 1);
 }
 
+// Function to calculate Fibonacci sequence
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main() {
     int num;
+    
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    printf("Factorial of %d = %d\n", num, factorial(num));
+    // Calculate factorial
+    int factorial_result = factorial(num);
+
+    printf("Factorial of %d = %d\n", num, factorial_result);
+
+    // Calculate Fibonacci sequence
+    printf("Fibonacci sequence up to %d terms:\n", num);
+    for (int i = 0; i < num; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+    
     return 0;
 }
