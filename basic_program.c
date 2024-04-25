e7eac226c12291d6664efba1d0fc8523cafca771
asifmahmud35 <asif.mahmud@brainstation-23.com>
2024-04-25 13:42:43 +0600

    Add basic programming problem

--- basic_program.c
+++ basic_program.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+// Function to calculate factorial
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
 int main() {
-    printf("Hello, world!\n");
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d = %d\n", num, factorial(num));
     return 0;
 }
